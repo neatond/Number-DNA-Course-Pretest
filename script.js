6d3a2c437b2fc85e1889f331bf29ee83919bc43f
Tommy <TommyCohn116@gmail.com>
2017-05-26 02:54:08 -0400

    Added a better way to organize some parts of the page object.

--- script.js
+++ script.js
@@ -19,25 +19,38 @@ var page = {};
 function setup() {
 	console.log("FUNCTION CALL: setup()");
 
-	page.CountBy3s = document.getElementById("CountBy3s");
-	page.Multiplication = document.getElementById("Multiplication");
-	page.Division = document.getElementById("Division");
-	page.Divisibility = document.getElementById("Divisibility");
-	page.Factoring = document.getElementById("Factoring");
-	page.PrimeFactorization = document.getElementById("PrimeFactorization");
-	page.LeastCommonMultiple = document.getElementById("LeastCommonMultiple");
-	page.GreatestCommonFactor = document.getElementById("GreatestCommonFactor");
-	page.AddingAndSubtractingIntegers = document.getElementById("AddingAndSubtractingIntegers");
-	page.MultiplyingAndDividingIntegers = document.getElementById("MultiplyingAndDividingIntegers");
-	page.TheNumberGame = document.getElementById("TheNumberGame");
-	page.EquivalentFractions = document.getElementById("EquivalentFractions");
-	page.Reducing = document.getElementById("Reducing");
-	page.ProperAndImproperFractionsAndMixedNumbers = document.getElementById("ProperAndImproperFractionsAndMixedNumbers");
-	page.AddingAndSubtractingFractions = document.getElementById("AddingAndSubtractingFractions");
-	page.MultiplyingAndDividingFractions = document.getElementById("MultiplyingAndDividingFractions");
-	page.PositiveAndNegativeExponents = document.getElementById("PositiveAndNegativeExponents");
-	page.ScientificNotation = document.getElementById("ScientificNotation");
+	page.navigation = {};
+
+	page.navigation.CountBy3s = document.getElementById("CountBy3s");
+	page.navigation.Multiplication = document.getElementById("Multiplication");
+	page.navigation.Division = document.getElementById("Division");
+	page.navigation.Divisibility = document.getElementById("Divisibility");
+	page.navigation.Factoring = document.getElementById("Factoring");
+	page.navigation.PrimeFactorization = document.getElementById("PrimeFactorization");
+	page.navigation.LeastCommonMultiple = document.getElementById("LeastCommonMultiple");
+	page.navigation.GreatestCommonFactor = document.getElementById("GreatestCommonFactor");
+	page.navigation.AddingAndSubtractingIntegers = document.getElementById("AddingAndSubtractingIntegers");
+	page.navigation.MultiplyingAndDividingIntegers = document.getElementById("MultiplyingAndDividingIntegers");
+	page.navigation.TheNumberGame = document.getElementById("TheNumberGame");
+	page.navigation.EquivalentFractions = document.getElementById("EquivalentFractions");
+	page.navigation.ReducingFractions = document.getElementById("ReducingFractions");
+	page.navigation.ProperAndImproperFractionsAndMixedNumbers = document.getElementById("ProperAndImproperFractionsAndMixedNumbers");
+	page.navigation.AddingAndSubtractingFractions = document.getElementById("AddingAndSubtractingFractions");
+	page.navigation.MultiplyingAndDividingFractions = document.getElementById("MultiplyingAndDividingFractions");
+	page.navigation.PositiveAndNegativeExponents = document.getElementById("PositiveAndNegativeExponents");
+	page.navigation.ScientificNotation = document.getElementById("ScientificNotation");
 	page.questionArea = document.getElementById("questionArea");
+
+	for(var element in page.navigation) {
+		page.navigation[element].addEventListener("click", function() {
+			loadQuestionTemplate(element);
+		});
+	}
+}
+function loadQuestionTemplate(element) {
+	console.log("FUNCTION CALL: loadQuestionTemplate("+element+")");
+
+	
 }
 
 //----------------------------------------------------------------
