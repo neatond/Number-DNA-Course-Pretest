568783b2000eacb61f5b83ee3ebd1249964ecfc1
Tommy <TommyCohn116@gmail.com>
2017-05-26 02:44:38 -0400

    Grab html elements from the page.

---
+++ script.js
@@ -0,0 +1,45 @@
+//----------------------------------------------------------------
+// CONSTANTS
+//----------------------------------------------------------------
+
+//----------------------------------------------------------------
+// GLOBAL VARIABLES
+//----------------------------------------------------------------
+
+var page = {};
+
+//----------------------------------------------------------------
+// CLASSES
+//----------------------------------------------------------------
+
+//----------------------------------------------------------------
+// FUNCTIONS
+//----------------------------------------------------------------
+
+function setup() {
+	console.log("FUNCTION CALL: setup()");
+
+	page.CountBy3s = document.getElementById("CountBy3s");
+	page.Multiplication = document.getElementById("Multiplication");
+	page.Division = document.getElementById("Division");
+	page.Divisibility = document.getElementById("Divisibility");
+	page.Factoring = document.getElementById("Factoring");
+	page.PrimeFactorization = document.getElementById("PrimeFactorization");
+	page.LeastCommonMultiple = document.getElementById("LeastCommonMultiple");
+	page.GreatestCommonFactor = document.getElementById("GreatestCommonFactor");
+	page.AddingAndSubtractingIntegers = document.getElementById("AddingAndSubtractingIntegers");
+	page.MultiplyingAndDividingIntegers = document.getElementById("MultiplyingAndDividingIntegers");
+	page.TheNumberGame = document.getElementById("TheNumberGame");
+	page.EquivalentFractions = document.getElementById("EquivalentFractions");
+	page.Reducing = document.getElementById("Reducing");
+	page.ProperAndImproperFractionsAndMixedNumbers = document.getElementById("ProperAndImproperFractionsAndMixedNumbers");
+	page.AddingAndSubtractingFractions = document.getElementById("AddingAndSubtractingFractions");
+	page.MultiplyingAndDividingFractions = document.getElementById("MultiplyingAndDividingFractions");
+	page.PositiveAndNegativeExponents = document.getElementById("PositiveAndNegativeExponents");
+	page.ScientificNotation = document.getElementById("ScientificNotation");
+	page.questionArea = document.getElementById("questionArea");
+}
+
+//----------------------------------------------------------------
+// EXECUTED CODE
+//----------------------------------------------------------------
